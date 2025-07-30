3421fad8076238712d0cc18ae430da669b2c6f1f
ProFMJ3 <follykoejules@gmail.com>
2025-07-30 23:07:52 +0000

    first commit

---
+++ assigment1.py
@@ -0,0 +1,12 @@
+
+
+#recupration des données
+
+print("Bienvenue dans notre système de calcul de la somme de deux nombres")
+
+nombre1 = int(input("Siasir le premier nombre(nombre 1) : "))
+nombre2 = int(input("Siasir le second nombre(nombre 2) : "))
+
+somme = nombre1 + nombre2
+
+print("Voici le résuslat de la somme de " , nombre1, " + ", nombre2, " = ", somme)
